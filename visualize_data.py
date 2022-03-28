04d163f4fd809e1e832413f5d024c472e10d4fc4
Aditi Vinod <aditi.b.vinod@gmail.com>
2022-03-28 13:39:06 -0400

    CSV extraction & creation added to scrape data. Data visualization word cloud function added.

--- visualize_data.py
+++ visualize_data.py
@@ -1,2 +1,14 @@
+#from PIL import Image
+#import numpy as np
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
 def got_bitches(result):
     print("You have no bitches")
+
+def word_cloud(frequency_dict):
+    wc = WordCloud(background_color="white").generate_from_frequencies(frequency_dict)
+    plt.figure(figsize = (10,10))
+    plt.imshow(wc, interpolation='bilinear')
+    plt.axis(False)
+    plt.show()
