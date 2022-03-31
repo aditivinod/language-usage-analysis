9a42760a086fb0fed71759d95e22058a173a3d87
Aditi Vinod <aditi.b.vinod@gmail.com>
2022-03-31 15:29:48 -0400

    test_gamer_words comments

--- test_gamer_words.py
+++ test_gamer_words.py
@@ -18,30 +18,30 @@ from gamer_words import (
 # Define sets of test cases.
 find_most_frequent_cases = [
     # Check that the simplest dictionary case returns correctly
-    ({"cheese":1}, 1, {"cheese":1}),
+    ({"cheese": 1}, 1, {"cheese": 1}),
     # Check that asking for no items returns an empty dictionary
-    ({"pedestrian":1}, 0, {}),
+    ({"pedestrian": 1}, 0, {}),
     # Check that a dictionary of 2 items returns items in the correct order
-    ({"cheese":1,"pedestrian":3,}, 2, {"pedestrian":3,"cheese":1}),
+    ({"cheese": 1, "pedestrian": 3, }, 2, {"pedestrian": 3, "cheese": 1}),
     # Check that the function can exclude cases if less words are asked for than
     # are in the dictionary
-    ({"cheese":1,"cheezits":4,"cheetos":7}, 2, {"cheetos":7,"cheezits":4})
-    
+    ({"cheese": 1, "cheezits": 4, "cheetos": 7}, 2, {"cheetos": 7, "cheezits": 4})
 
-    
+
+
 ]
 
 instances_to_decimal_cases = [
     # Check that the simplest case of the function works
-    ({"cheese":1},{"cheese":1},1),
+    ({"cheese": 1}, {"cheese": 1}, 1),
     # Check that single word inputs always result in one
-    ({"cheese":10101},{"cheese":1},10101),
+    ({"cheese": 10101}, {"cheese": 1}, 10101),
     # Check that a simple dictionary with multiple words with positive integer
     # values functions
-    ({"cheese":1,"malt":2},{"cheese":1/3,"malt":2/3},3),
+    ({"cheese": 1, "malt": 2}, {"cheese": 1/3, "malt": 2/3}, 3),
     # Check that if the sum of the values of the dictionary is 0, then the
     # function returns an empty dictionary
-    ({"cheese":0,"malt":0},{},0)
+    ({"cheese": 0, "malt": 0}, {}, 0)
 
 
 ]
@@ -49,18 +49,18 @@ instances_to_decimal_cases = [
 remove_most_common_cases = [
     # Check that a blank normal dictionary does not remove any words from the
     # gamer dictionary
-    ({},{"game":.1,"gaming":.6,"poggers":.3},0,1 \
-        ,{},{"game":.1,"gaming":.6,"poggers":.3},[]),
-    
-    # Check a simple case where a word gets removed
-    ({"game":.13},{"game":.1,"gaming":.7,"poggers":.5},1,1,\
-        {},{"gaming":.7/1.2,"poggers":.5/1.2},["game"]),
+    ({}, {"game": .1, "gaming": .6, "poggers": .3}, 0, 1,
+     {}, {"game": .1, "gaming": .6, "poggers": .3}, []),
 
-    # Check more complex case where some words get removed, some words are 
-    # different in each dictionary and the total number of words in each 
+    # Check a simple case where a word gets removed
+    ({"game": .13}, {"game": .1, "gaming": .7, "poggers": .5}, 1, 1,\
+        {}, {"gaming": .7/1.2, "poggers": .5/1.2}, ["game"]),
+
+    # Check more complex case where some words get removed, some words are
+    # different in each dictionary and the total number of words in each
     # dictionary is both not one and not the same
-    ({"game":.13,"poggers":1,"gaming":.65,"soul":1},{"game":.1,"gaming":.7,"salmon":.4,"poggers":.6},3,4,\
-        {"poggers":1/(1 + 1),"soul":1/(1 + 1)},{"salmon":.4/1,"poggers":.6/1},["game","gaming"]),
+    ({"game": .13, "poggers": 1, "gaming": .65, "soul": 1}, {"game": .1, "gaming": .7, "salmon": .4, "poggers": .6}, 3, 4,\
+        {"poggers": 1/(1 + 1), "soul": 1/(1 + 1)}, {"salmon": .4/1, "poggers": .6/1}, ["game", "gaming"]),
 
 
 
@@ -70,45 +70,47 @@ remove_most_common_cases = [
 remove_too_uncommon_cases = [
     # Test a simple case with clearly removable entries and a threshold equal to
     # one of the values of a key
-   ({"gamers":1,"pasta":24,"notgamer":4,"gamingaminggaming":2},4,\
-       {"pasta":24,"notgamer":4}),
+    ({"gamers": 1, "pasta": 24, "notgamer": 4, "gamingaminggaming": 2}, 4,\
+     {"pasta": 24, "notgamer": 4}),
     # Test that keys of the form "wordcword" get removed
-    ({"mochacmocha":101010101,"gamers":6,"aditicaditi":85,"lukecaditi":16,\
-        "pastacpasta":2},3,{"gamers":6,"lukecaditi":16}),
+    ({"mochacmocha": 101010101, "gamers": 6, "aditicaditi": 85, "lukecaditi": 16,\
+        "pastacpasta": 2}, 3, {"gamers": 6, "lukecaditi": 16}),
     # Check that overly long entries are removed
-    ({"asfdljkasfdladsfjksfdakhasfdlfsda":110},2,{}),
+    ({"asfdljkasfdladsfjksfdakhasfdlfsda": 110}, 2, {}),
     # Check an empty dictionary
-    ({},0,{})
+    ({}, 0, {})
 
-    
+
 
 ]
 
 determine_gamer_words_cases = [
     # Check a simple case with a gamer word that is used drastically more in the
     # Gamer dictionary than the normal dictionary
-    ({"gaming":.01,"pogggg":.01},{"gaming":.1,"pogggg":.05},["gaming"]),
+    ({"gaming": .01, "pogggg": .01}, {
+     "gaming": .1, "pogggg": .05}, ["gaming"]),
     # Check that a gamer word that isn't in the normal dictionary makes the list
-    ({"blame":.152},{"blame":.153,"sled":.000078,"sleigh":.00008},["sleigh"]),
+    ({"blame": .152}, {"blame": .153,
+     "sled": .000078, "sleigh": .00008}, ["sleigh"]),
     # Check that a word that shows up a substantially different amount in the
     # normal and gamer dictionaries but is used less in the gamer dictionary is
     # not considered a gamer word
-    ({"fantasy":1},{"fantasy":.000000000000001},[])
+    ({"fantasy": 1}, {"fantasy": .000000000000001}, [])
 ]
 
 # The parse_words function is composed entirely of the previous functions and
-# thus does not need unit tests, or rather the unit tests for this function 
+# thus does not need unit tests, or rather the unit tests for this function
 # would fundamentally test the same things as the previous unit tests
 
 determine_language_similarity_cases = [
     # Check that an empty language dictionary outputs the sum of the squares
     # of the user dictionary
-    ({},{"gaming":3,"gamers":4},5),
+    ({}, {"gaming": 3, "gamers": 4}, 5),
     # Theck that if the user and language dictionaries are the same that
     # the result is 0
-    ({"gaming":3,"gamers":4},{"gaming":3,"gamers":4},0),
+    ({"gaming": 3, "gamers": 4}, {"gaming": 3, "gamers": 4}, 0),
     # Check a case with words in both dictionaries but with with unequal values
-    ({"gaming":2,"gamers":4},{"gaming":5,"gamers":8},5),
+    ({"gaming": 2, "gamers": 4}, {"gaming": 5, "gamers": 8}, 5),
 ]
 
 # The parse_words function does not have any unit tests written for it as it
@@ -130,8 +132,8 @@ determine_language_similarity_cases = [
 
 is_gamer_cases = [
     # Check that a negative and a positive z-value return True
-    
-  
+
+
 
 ]
 
@@ -156,7 +158,7 @@ def test_find_most_frequent(freq_input_dict, freq_input_int, output_dict):
 
 @pytest.mark.parametrize("input_dictionary,output_dictionary,total_words",
                          instances_to_decimal_cases)
-def test_instances_to_decimal(input_dictionary, output_dictionary,total_words):
+def test_instances_to_decimal(input_dictionary, output_dictionary, total_words):
     """
     Test that the instance_to_decimal function properly converts from integers
     to floats.
@@ -171,13 +173,14 @@ def test_instances_to_decimal(input_dictionary, output_dictionary,total_words):
             set.
 
     """
-    assert instances_to_decimal(input_dictionary) == (output_dictionary,total_words)
+    assert instances_to_decimal(input_dictionary) == (
+        output_dictionary, total_words)
 
 
 @pytest.mark.parametrize("normal_dictionary,gamer_dictionary,normal_total_words,gamer_total_words,normal_dict_out,\
 gamer_dict_out,ignore_list_out", remove_most_common_cases)
-def test_remove_most_common(normal_dictionary, gamer_dictionary,normal_total_words,gamer_total_words,normal_dict_out,\
-    gamer_dict_out,ignore_list_out):
+def test_remove_most_common(normal_dictionary, gamer_dictionary, normal_total_words, gamer_total_words, normal_dict_out,
+                            gamer_dict_out, ignore_list_out):
     """
     Test that the remove_most_common function removes the words that it should
     be removing.
@@ -203,11 +206,12 @@ def test_remove_most_common(normal_dictionary, gamer_dictionary,normal_total_wor
             from both dictionaries.
 
     """
-    assert remove_most_common(normal_dictionary, gamer_dictionary,normal_total_words,gamer_total_words) == \
-        (normal_dict_out,gamer_dict_out,ignore_list_out)
+    assert remove_most_common(normal_dictionary, gamer_dictionary, normal_total_words, gamer_total_words) == \
+        (normal_dict_out, gamer_dict_out, ignore_list_out)
+
 
 @pytest.mark.parametrize("dictionary,threshold,dictionary_out", remove_too_uncommon_cases)
-def test_remove_too_uncommon(dictionary,threshold,dictionary_out):
+def test_remove_too_uncommon(dictionary, threshold, dictionary_out):
     """
 
     Test that remove_too_uncommon removes typos, overly long strings, and words
@@ -223,11 +227,11 @@ def test_remove_too_uncommon(dictionary,threshold,dictionary_out):
             words and integers as values representing how many times that word
             is used in a dataset.
     """
-    assert remove_too_uncommon(dictionary,threshold) == dictionary_out
+    assert remove_too_uncommon(dictionary, threshold) == dictionary_out
 
 
 @pytest.mark.parametrize("normal_dictionary,gamer_dictionary,word_list", determine_gamer_words_cases)
-def test_determine_gamer_words(normal_dictionary, gamer_dictionary,word_list):
+def test_determine_gamer_words(normal_dictionary, gamer_dictionary, word_list):
     """
     Test that the determine_gamer_words() function properly finds language
     very commonly used by gamers.
@@ -241,13 +245,14 @@ def test_determine_gamer_words(normal_dictionary, gamer_dictionary,word_list):
             used in the dataset.
         word_list: A list of strings representing words specific to the
             gamer vocabulary.
-    
+
     """
-    assert determine_gamer_words(normal_dictionary,gamer_dictionary) == word_list
+    assert determine_gamer_words(
+        normal_dictionary, gamer_dictionary) == word_list
 
 
 @pytest.mark.parametrize("language_dict,user_dict,closeness_value", determine_language_similarity_cases)
-def test_determine_language_similarity(language_dict, user_dict,closeness_value):
+def test_determine_language_similarity(language_dict, user_dict, closeness_value):
     """
     Test that the determine_language_similarity() function properly determines
     closeness values between user and community language datasets.
@@ -262,7 +267,8 @@ def test_determine_language_similarity(language_dict, user_dict,closeness_value)
         closeness_value: A float representing how close a user's total language 
             usage is to a given set of data with lower numbers being closer.
     """
-    assert determine_language_similarity(language_dict,user_dict) == closeness_value
+    assert determine_language_similarity(
+        language_dict, user_dict) == closeness_value
 
 
 @pytest.mark.parametrize("strand,orf", is_gamer_cases)
@@ -274,4 +280,3 @@ def test_is_gamer(gamer_z, normal_z, gamer_status):
 
     """
     assert is_gamer(gamer_z, normal_z) == gamer_status
-
